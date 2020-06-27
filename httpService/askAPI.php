dfcbc7293e1b3635d527f6cc88812772ec6d3d7c
whitefancy <whitefancy@msn.cn>
2020-06-27 22:42:26 +0800

    变量作用域

---
+++ httpService/askAPI.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * [http 调用接口函数]
+ * @param  string       $url     [接口地址]
+ * @param  array        $params  [数组]
+ * @param  string       $method  [GET\POST\DELETE\PUT]
+ * @param  array        $header  [HTTP头信息]
+ * @param  integer      $timeout [超时时间]
+ * @return [type]                [接口返回数据]
+ */
+function http($url, $params, $method = 'GET', $header = array(), $timeout = 5)
+{
+    // POST 提交方式的传入 $set_params 必须是字符串形式
+    $opts = array(
+        CURLOPT_TIMEOUT => $timeout,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_HTTPHEADER => $header
+    );
+
+    /* 根据请求类型设置特定参数 */
+    switch (strtoupper($method)) {
+        case 'GET':
+            $opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
+            break;
+        case 'POST':
+            $params = http_build_query($params);
+            $opts[CURLOPT_URL] = $url;
+            $opts[CURLOPT_POST] = 1;
+            $opts[CURLOPT_POSTFIELDS] = $params;
+            break;
+        case 'DELETE':
+            $opts[CURLOPT_URL] = $url;
+            $opts[CURLOPT_HTTPHEADER] = array("X-HTTP-Method-Override: DELETE");
+            $opts[CURLOPT_CUSTOMREQUEST] = 'DELETE';
+            $opts[CURLOPT_POSTFIELDS] = $params;
+            break;
+        case 'PUT':
+            $opts[CURLOPT_URL] = $url;
+            $opts[CURLOPT_POST] = 0;
+            $opts[CURLOPT_CUSTOMREQUEST] = 'PUT';
+            $opts[CURLOPT_POSTFIELDS] = $params;
+            break;
+        default:
+            throw new Exception('不支持的请求方式！');
+    }
+
+    /* 初始化并执行curl请求 */
+    $ch = curl_init();
+    curl_setopt_array($ch, $opts);
+    $data = curl_exec($ch);
+    $error = curl_error($ch);
+    echo $error;
+    return $data;
+}
